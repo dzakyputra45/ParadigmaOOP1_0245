fcba23231dea362c01103fce511b0200e42388a0
dzakyputra45 <dzakypratama45@gmail.com>
2024-05-21 16:03:20 +0700

    Menambahkan class ibu di dalam agregasi

---
+++ Aggregasi/Ibu.h
@@ -0,0 +1,29 @@
+#ifndef IBU_H
+#define IBU_H
+
+class ibu {
+public:
+    string nama;
+    vector<anak*> daftar_anak;
+
+    Ibu(string pNama) :nama(pNama) {
+        cout << "Ibu \"" << nama "\" ada\n";
+    }
+
+    ~Ibu() {
+        cout << "Ibu \"" << nama << "\" tidak ada\n";
+    }
+    void tambahAnak(anak*);
+    void cetakAnak();
+};
+void Ibu::tambahAnak(anak* pAnak) {
+	daftar_anak.push_back(pAnak); 
+}
+void ibu::cetakAnak() {
+	cout << "Daftar Anak dari Ibu \"" << this->nama << "\":\n"; 
+	for (auto& a : daftar_anak) {
+		cout << a->nama << "\n"; 
+	}
+	cout << endl;
+}
+#endif
